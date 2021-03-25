331fccba08dccc1f13401260e930e1c41ca7d1d8
DanielDrechsel <ddd9@njit.edu>
2021-03-25 11:13:06 -0400

    fixing file structure, adding new controllers and tests, added login page with css

--- src/main/java/com/theultimatelist/AccessingDataMysqlApplication.java
+++ src/main/java/com/theultimatelist/AccessingDataMysqlApplication.java
@@ -1,4 +1,4 @@
-package com.theultimatelist.backend.database;
+package com.theultimatelist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AccessingDataMysqlApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AccessingDataMysqlApplication.class, args);
     }
 
---
+++ src/main/java/com/theultimatelist/controllers/HelloRestController.java
@@ -0,0 +1,17 @@
+package com.theultimatelist.controllers;
+
+import com.theultimatelist.entities.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+    @GetMapping("/rest")
+    public Greeting greet(@RequestParam( required = false, defaultValue = "World") String name){
+        return new Greeting(String.format("Hello, %s!" , name ));
+
+
+    }
+}
